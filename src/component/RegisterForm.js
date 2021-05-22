233c01667153805b600dbd2b5326918100516763
Saif Maknojia <saif.maknojia@outlook.com>
2021-05-22 15:40:58 -0400

    Register form implemented

--- src/component/RegisterForm.js
+++ src/component/RegisterForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Form, Button, Container } from 'react-bootstrap';
 
-const SignIn = () => {
+const RegisterForm = () => {
   const style = {
     width: '400px',
     maxWidth: '600px'
@@ -10,12 +10,11 @@ const SignIn = () => {
   const handleSubmit = e => {
     e.preventDefault();
   };
-
   return (
-    <Container style={style} className="mb-5">
-      <h2>Please Sign In</h2>
+    <Container style={style} className=" mb-5">
+      <h3>Register</h3>
       <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formBasicEmail1">
+        <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
           <Form.Text className="text-muted">
@@ -23,17 +22,25 @@ const SignIn = () => {
           </Form.Text>
         </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
+        <Form.Group className="mt-1" controlId="formBasicPassword1">
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" />
         </Form.Group>
-
-        <Button variant="primary" type="submit" className="mt-4">
-          Sign In
+        <Form.Group className="mt-2" controlId="formBasicPassword2">
+          <Form.Label>Re-Enter Password</Form.Label>
+          <Form.Control type="password1" placeholder="Re- Enter Password" />
+        </Form.Group>
+        <Button
+          className="mt-4"
+          variant="primary"
+          type="submit"
+          onSubmit={handleSubmit}
+        >
+          Register
         </Button>
       </Form>
     </Container>
   );
 };
 
-export default SignIn;
+export default RegisterForm;
--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import RegisterForm from '../component/RegisterForm';
 import SignIn from '../component/SignIn';
 
 const SignInPage = () => {
   return (
     <div className="container mt-5">
-      <h2>Please Sign In</h2>
-      <SignIn />
+      <div className="d-flex flex-wrap">
+        <SignIn />
+        <RegisterForm />
+      </div>
     </div>
   );
 };
